e61251a07df074a3b6a0ee88d9d403206a5c29f3
Arunkumar <118586803+arunkumar204@users.noreply.github.com>
2023-10-22 09:56:02 +0530

    Update settings.py

--- Weather-App/weatherproject/settings.py
+++ Weather-App/weatherproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'weatherapp.apps.WeatherappConfig',
+    'weatherapp',
 ]
 
 MIDDLEWARE = [
